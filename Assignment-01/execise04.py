d71a4318ef54ea655ba43086dbb467cf2c2603e3
Levindo Neto <levindogtn@gmail.com>
2017-11-06 20:10:34 +0100

    Add Assignment 01

---
+++ Assignment-01/execise04.py
@@ -0,0 +1,13 @@
+''' Exercise 4 of the task 1 '''
+''' Group members: 
+ 	Amelie Fritsch, 3332267
+ 	Hana Kang, 3247297
+ 	Levindo Gabriel Taschetto Neto, 3292080
+ 	Vanessa Hindinger, 3324440
+'''
+
+x = input("Enter number 1:\n")
+y = input("Enter number 2:\n")
+
+print("Sum: ", int(x)+int(y))
+print("Product: ", int(x)*int(y))
